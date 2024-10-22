4798d5112eec429fd38ed456b2439135ab801a02
Baptiste P <baptiste.pauchet13@gmail.com>
2024-10-22 13:03:50 +0200

    Ajout de tout la partie debugage dans le main

--- main.py
+++ main.py
@@ -3,55 +3,74 @@ from travailJSON import *
 def main():
     jsonTravail = jsonWork("testFile/test.json")
     var = 0
-    while (var < 19) :
+    while (var < 18) :
         verifSortie = False
         while verifSortie == False :
             sortieUser = input("1.getContenuJSON\n2.lectureJSON\n3.lectureJSONMultiFlag\n"
                                "4.lectureJSONList\n5.lectureJSONDict\n6.EcritureJSON\n"
-                               "7.EcritureJSONList\n8.EcritureJSONDictionnaire\n9.supprJSONList\n"
-                               "10.suppressionJson\n11.uppressionJsonList\n12.dictJson\n"
-                               "13.ompteurFlagJSON\n14.supprDictReorg\n15.creerFlagDictionnaire\n"
-                               "16.ajouterFlagDict\n17.supprDictByFlag\n18.writeDictOnJson\n"
-                               "19.Quit\n$ ")
+                               "7.EcritureJSONList\n8.EcritureJSONDictionnaire\n9.supprJSONDict\n"
+                               "10.suppressionJson\n11.suppressionJsonList\n"
+                               "12.compteurFlagJSON\n13.supprDictReorg\n14.creerFlagDictionnaire\n"
+                               "15.ajouterFlagDict\n16.supprDictByFlag\n17.writeDictOnJson\n"
+                               "18.Quit\n$ ")
             verifSortie = sortieUser.isdigit()
         var = int(sortieUser)
         match var :
             case 1 :
-                jsonTravail
+                print(jsonTravail.getContenuJSON())
             case 2 :
-                jsonTravail
+                print(jsonTravail.lectureJSON("test"))
             case 3 :
-                jsonTravail
+                print(jsonTravail.lectureJSONMultiFlag("testM","test1"))
             case 4:
-                jsonTravail
+                print(jsonTravail.lectureJSONList("testList"))
             case 5:
-                jsonTravail
+                print(jsonTravail.lectureJSON("testM"))
             case 6:
-                jsonTravail
+                flag = input("Entrer le flag : ")
+                valeur = input("Enter la valeur :")
+                jsonTravail.EcritureJSON(flag,valeur)
             case 7:
-                jsonTravail
+                flag = input("Entrer le flag : ")
+                valeur = input("Enter la valeur :")
+                jsonTravail.EcritureJSONList(flag,valeur)
             case 8:
-                jsonTravail
+                flag = input("Entrer le flag : ")
+                valeur = input("Enter la valeur :")
+                jsonTravail.EcritureJSONDictionnaire("testM",flag,valeur)
             case 9:
-                jsonTravail
+                flag = input("Entrer le flag : ")
+                jsonTravail.supprJSONDict("testM",flag)
             case 10:
-                jsonTravail
+                flag = input("Entrer le flag : ")
+                jsonTravail.suppressionJson(flag)
             case 11:
-                jsonTravail
+                flag = input("Entrer le flag : ")
+                valeur = input("Enter la valeur :")
+                jsonTravail.suppressionJsonList(flag,valeur)
             case 12:
-                jsonTravail
+                print(jsonTravail.compteurFlagJSON())
             case 13:
-                jsonTravail
+                flag = input("Entrer le flag : ")
+                jsonTravail.supprDictReorg(flag)
             case 14:
-                jsonTravail
+                flag = input("Entrer le flag : ")
+                jsonTravail.creerFlagDictionnaire(flag)
             case 15:
-                jsonTravail
+                flag = input("Entrer le flag : ")
+                cles = input("Entrer la cles : ")
+                valeur = input("Enter la valeur :")
+                jsonTravail.ajouterFlagDict(flag,cles,valeur)
             case 16:
-                jsonTravail
+                flag = input("Entrer le flag : ")
+                name = input("Entrer le nom : ")
+                jsonTravail.supprDictByFlag(flag,name)
             case 17:
-                jsonTravail
-            case 18:
-                jsonTravail
+                dict = {"a":"b","b":"b","c":"b","d":"b","e":"b","f":"b"}
+                jsonTravail.writeDictOnJson(dict)
+
+        if (var != 18):
+            input("Press enter")
 
     print("Au revoir")
 
